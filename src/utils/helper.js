aaad319f3cbdcb537330ac181c7ea9ebda64429e
Kalu Nwanna <nwannauk@gmail.com>
2021-03-18 05:46:36 +0100

    fix error

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -11,40 +11,15 @@ export const updateCart = (cartList, product, action) => {
     return [...cartList, newProduct]
   } 
   const selectedProduct = {...cartList[indexOfProduct]}
-  const incrementProduct = {selectedProduct, count: selectedProduct.count + 1};
-  const decrementProduct = {selectedProduct, count: selectedProduct.count - 1};
+  const incrementProduct = {...selectedProduct, count: selectedProduct.count + 1};
+  const decrementProduct = {...selectedProduct, count: selectedProduct.count - 1};
   if (selectedProduct.count === 1 && action === "decrement") {
-    return [
-      ...cartList.slice(0, indexOfProduct),
-      ...cartList.slice(indexOfProduct + 1),
-    ];
-  }
-  if(action === "increment") {
-    return [
-      ...cartList.slice(0, indexOfProduct), incrementProduct
-      ]
+    return cartList.filter((_, i) => i !== indexOfProduct);
   }
   if (action === "decrement") {
     return [
-      ...cartList.slice(0, indexOfProduct), decrementProduct
+      ...cartList.slice(0, indexOfProduct), decrementProduct, ...cartList.slice(indexOfProduct + 1)
       ]
   }
-  
+  return [...cartList.slice(0, indexOfProduct), incrementProduct, ...cartList.slice(indexOfProduct + 1)]
 }
-
-/**
- * @description Removes product from cart and returns the new cart
- * @param {Array} cartList the list of products in the cart 
- * @param {Object} product the new selected product to be added
- */
-export const removeProductFromCart = (cartList, product) => {
-  const indexOfProduct = cartList.findIndex((cartItem) => cartItem.id === product.id);
-    if (indexOfProduct === -1) {
-      return cartList;
-    }
-    return [
-      ...cartList.slice(0, indexOfProduct),
-      ...cartList.slice(indexOfProduct + 1),
-    ];
-}
-
